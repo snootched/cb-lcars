0753215bdfd84ad5e2ce06ab37e643d534739ea7
Jay <snootched@gmail.com>
2024-08-24 20:31:24 -0400

    add logging function

--- dist/cb-lcars.js
+++ dist/cb-lcars.js
@@ -7,16 +7,37 @@ document.head.appendChild(script);
 
 script.onload = () => {
 
+    async function cblcarsLog(level, message) {
+        let styles = [
+            'color: white',
+            'padding: 2px 4px',
+            'border-radius: 15px'
+        ];
 
-    let styles = [
-        'color: white',
-        'padding: 2px 4px',
-        'border-radius: 15px',
-        'background-color: #37a6d1'
-    ];
-
-    console.log(`%c    CB-LCARS | info `, styles.join(';'), 'js-yaml.min loaded');
-
+        switch (level) {
+            case 'info':
+                styles.push('background-color: #37a6d1'); // Blue
+                console.log(`%c    CB-LCARS | ${level} `, styles.join(';'), message);
+                break;
+            case 'warn':
+                styles.push('background-color: #ff6753'); // Orange
+                console.warn(`%c    CB-LCARS | ${level} `, styles.join(';'), message);
+                break;
+            case 'error':
+                styles.push('background-color: #ef1d10'); // Red
+                console.error(`%c    CB-LCARS | ${level} `, styles.join(';'), message);
+                break;
+            case 'debug':
+                styles.push('background-color: #8e44ad'); // Purple
+                console.debug(`%c    CB-LCARS | ${level} `, styles.join(';'), message);
+                break;
+            default:
+                styles.push('background-color: #6d748c'); // Gray for unknown levels
+                console.log(`%c    CB-LCARS | ${level} `, styles.join(';'), message);
+                break;
+        }
+    }
+ 
 
     async function fetchYAML(url) {
         try {
@@ -38,11 +59,21 @@ script.onload = () => {
     fetchYAML(url)
         .then(yaml => {
             const jsObject = jsyaml.load(yaml);
-            console.log(jsObject);
+            cblcarsLog('info',`fetched and parsed yaml ${url}`);
+            cblcarsLog('debug',jsObject);
 
             // Define the CustomStrategy class
             class CustomStrategy {
                 static async generate(config, hass) {
+                    const [areas, devices, entities] = await Promise.all([
+                        hass.callWS({ type: "config/area_registry/list" }),
+                        hass.callWS({ type: "config/device_registry/list" }),
+                        hass.callWS({ type: "config/entity_registry/list" }),
+                      ]);
+                    console.log(areas);
+                    console.log(devices);
+                    console.log(entities);
+                      
                     return {
                         title: 'CB-LCARS',
                         ...jsObject, // Use the parsed YAML content here
