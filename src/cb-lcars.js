b4b0b9ec5e0c5179a0e6030f468c2ebf98df802f
Jay <snootched@gmail.com>
2024-09-01 16:46:30 -0400

    fix comparison

--- src/cb-lcars.js
+++ src/cb-lcars.js
@@ -207,7 +207,7 @@ async function updateLovelaceConfig(filePath) {
 
     cblcarsLog('debug','updateLoveLaceConfig.newConfig: ',newConfig);
 
-    if (newConfig === undefined || newConfig === null) {
+    if (newConfig === undefined || newConfig === null || newConfig === 'undefined') {
         cblcarsLog('error','The CB-LCARS lovelace template failed and is not availalbe for processing.',error);
         throw error;
     } else {
