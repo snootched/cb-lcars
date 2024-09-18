bd7b938de6d21a5d831165cceaaa33b2eb378681
Jay <snootched@gmail.com>
2024-09-18 17:53:14 -0400

    cleanup

--- src/editor/cb-lcars-editor.js
+++ src/editor/cb-lcars-editor.js
@@ -1,10 +1,8 @@
-import jsyaml from 'js-yaml';
-
 import * as CBLCARS from '../cb-lcars-vars.js'
 import { cblcarsLog } from '../utils/cb-lcars-logging.js';
-import { fetchYAML, readYamlFile } from '../utils/cb-lcars-fileutils.js';
+import { readYamlFile } from '../utils/cb-lcars-fileutils.js';
 
-import { html, css, unsafeCSS, CSSResult} from 'lit';
+import { html, css, unsafeCSS } from 'lit';
 
 import EditorForm from 'ha-editor-formbuilder';
 
@@ -14,45 +12,37 @@ export class CBLCARSCardEditor extends EditorForm {
     _formControls;
     _cardType;
 
-    /// !!!move to base class
-    //_userStyles;
-
     constructor(cardType) {
         super();
 
-        /// !!!move to base class
-        //this._userStyles = css``;
-        
         this._formDefinitions = {};
         this._formControls = {};
         this._cardType = "";
 
         this._cardType = cardType;
-        //this._cardType = config.type.replace(/^custom:/, '');    
 
-        cblcarsLog('debug',`cardType key for YAML config: ${cardType}`);
+        cblcarsLog('debug',`Setting up editor for ${cardType}`);
 
     }
 
     async setConfig(config) {
 
-        //let's get our this._config setup..
         super.setConfig(config);
 
-        cblcarsLog('debug','CBLCARSCardEditor.setConfig()  this._config:',this._config);
-        cblcarsLog('debug',`this._cardType key for YAML config: ${this._cardType}`);
+//        cblcarsLog('debug','CBLCARSCardEditor.setConfig()  this._config:',this._config);
+//        cblcarsLog('debug',`this._cardType key for YAML config: ${this._cardType}`);
         
         try {
             const formDefinitions = await readYamlFile(CBLCARS.card_editor_uri)
-            cblcarsLog('debug','formDefinitions: ',formDefinitions);
+//            cblcarsLog('debug','formDefinitions: ',formDefinitions);
             this._formDefinitions = formDefinitions;
-            console.debug('this._formDefinitions: ',this._formDefinitions)
+//            console.debug('this._formDefinitions: ',this._formDefinitions)
 
             //returns the content for this card type
             this._formControls = formDefinitions[this._cardType];
-            console.debug('this._formControls: ',this._formControls);
+//            console.debug('this._formControls: ',this._formControls);
 
-            this._userStyles = css`${unsafeCSS(formDefinitions[this._cardType].css.cssText || '')}`;
+            this._userStyles = css`${unsafeCSS((formDefinitions[this._cardType].css && formDefinitions[this._cardType].css.cssText) || '')}`;
             console.debug('this._userStyles: ',this._userStyles);
             this._mergeUserStyles = formDefinitions[this._cardType].css.mergeUserStyles !== undefined && formDefinitions[this._cardType].css.mergeUserStyles !== null ? formDefinitions[this._cardType].css.mergeUserStyles : false;
             console.debug('this._mergeUserStyles: ',this._mergeUserStyles);
@@ -63,62 +53,34 @@ export class CBLCARSCardEditor extends EditorForm {
         }
     }
 
-    updated(changedProperties) {
-        super.updated(changedProperties);
+//    updated(changedProperties) {
+//        super.updated(changedProperties);
         //if (changedProperties.has('_userStyles')) {
-            this.shadowRoot.adoptedStyleSheets = [this.constructor.styles.styleSheet, this._userStyles.styleSheet];
+//            this.shadowRoot.adoptedStyleSheets = [this.constructor.styles.styleSheet, this._userStyles.styleSheet];
         //}
-    }
+//    }
 
     render() {
-        if (!this._hass || !this._config || !this._formDefinitions) {
-            cblcarsLog('debug','Unable to setup form rendering - returning blank');
-            return html``;
+        if (!this._hass) {
+            return html`<ha-alert alert-type="error" title="Error">Home Assistant instance is missing.</ha-alert>`;
         }
-
-
-        const formContent = this._formControls;
-        cblcarsLog('debug',`Editor formContent: `,formContent);
-
+    
+        if (!this._config) {
+            return html`<ha-alert alert-type="error" title="Error">Card configuration is missing.</ha-alert>`;
+        }
+    
+        if (!this._formControls) {
+            return html`<ha-alert alert-type="error" title="Error">Form controls are missing.</ha-alert>`;
+        }
+  
         try {
+            const formContent = this._formControls;
             const returnForm = this.generateForm(formContent);
-            console.log('returnForm:', returnForm);
             return returnForm;
         } catch (error) {
-            console.error('Error in renderForm:', error);
-            return html`<p>Error rendering form</p>`;
+            cblcarsLog('error','Error rendering configuration form:', error);
+            return html`<ha-alert alert-type="error" title="Error">Error rendering form: ${error.message}</ha-alert>`;   
         }
     }
 
-
-/*  !!!move to base class 
-    static get styles() {
-        const baseStyles = super.styles;
-        return baseStyles;
-        //return [ baseStyles, this._userStyles ];
-    }
-*/
-
-
-
-
-
-
-    /*
-    static get styles() {
-        //if (!this._formStyles) {
-        if (!CBLCARSCardEditor._userStyles) {
-                cblcarsLog('debug','No editor form custom styles found for this card - returning base class css.');
-            return super.styles;
-        }
-
-        
-        cblcarsLog('debug',"formStyles: ", CBLCARSCardEditor._userStyles);
-        cblcarsLog('info',"Returning editor form styles for this card: ", CBLCARSCardEditor._formStyles);
-        return [
-            super.styles,
-            CBLCARSCardEditor._userStyles
-        ];
-    }
-    */
 }    
