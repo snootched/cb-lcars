f1dbb3e8f81e36bdc0fb659ad188d2280d9c0140
Jay <snootched@gmail.com>
2024-09-21 16:36:24 -0400

    try move yaml to constructor with init promise

--- src/editor/cb-lcars-editor.js
+++ src/editor/cb-lcars-editor.js
@@ -23,15 +23,39 @@ export class CBLCARSCardEditor extends EditorForm {
 
         cblcarsLog('debug',`Setting up editor for ${cardType}`);
 
+        this._initializationPromise = this._initialize();
+    }
+
+    async _initialize() {
+        try {
+            const formDefinitions = await readYamlFile(CBLCARS.card_editor_uri)
+            cblcarsLog('debug','formDefinitions: ',formDefinitions);
+            this._formDefinitions = formDefinitions;
+            console.debug('this._formDefinitions: ',this._formDefinitions)
+
+            //returns the content for this card type
+            this._formControls = formDefinitions[this._cardType];
+            console.debug('this._formControls: ',this._formControls);
+
+            this._userStyles = css`${unsafeCSS((formDefinitions[this._cardType].css && formDefinitions[this._cardType].css.cssText) || '')}`;
+            //console.debug('this._userStyles: ',this._userStyles);
+            this._mergeUserStyles = formDefinitions[this._cardType]?.css?.mergeUserStyles ?? true;
+            //console.debug('this._mergeUserStyles: ',this._mergeUserStyles);
+
+            this.requestUpdate();
+        } catch(error) {
+            cblcarsLog('error','Error fetching editor form definitions: ', error);
+        }
     }
 
     async setConfig(config) {
 
-        super.setConfig(config);
+        await this._initializationPromise;
 
-//        cblcarsLog('debug','CBLCARSCardEditor.setConfig()  this._config:',this._config);
-//        cblcarsLog('debug',`this._cardType key for YAML config: ${this._cardType}`);
-        
+        super.setConfig(config);
+        this.requestUpdate();
+
+        /*  ..try moving to constructor to read yaml file once
         try {
             const formDefinitions = await readYamlFile(CBLCARS.card_editor_uri)
 //            cblcarsLog('debug','formDefinitions: ',formDefinitions);
@@ -51,29 +75,31 @@ export class CBLCARSCardEditor extends EditorForm {
         } catch(error) {
             cblcarsLog('error','Error fetching editor form definitions: ', error);
         }
+        */
+
     }
 
     render() {
         if (!this._hass) {
             return html`<ha-alert alert-type="error" title="Error">Home Assistant instance is missing.</ha-alert>`;
         }
-    
+
         if (!this._config) {
             return html`<ha-alert alert-type="error" title="Error">Card configuration is missing.</ha-alert>`;
         }
-    
+
         if (!this._formControls) {
             return html`<ha-alert alert-type="error" title="Error">Form controls are missing.</ha-alert>`;
         }
-  
+
         try {
             const formContent = this._formControls;
             const returnForm = this.generateForm(formContent);
             return returnForm;
         } catch (error) {
             cblcarsLog('error','Error rendering configuration form:', error);
-            return html`<ha-alert alert-type="error" title="Error">Error rendering form: ${error.message}</ha-alert>`;   
+            return html`<ha-alert alert-type="error" title="Error">Error rendering form: ${error.message}</ha-alert>`;
         }
     }
 
-}    
+}
