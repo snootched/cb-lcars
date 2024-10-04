77dcc7e2b3b93ab0660ebcb453baaed1e196dcac
Jay <snootched@gmail.com>
2024-10-04 16:32:23 -0400

    browser version of path

--- src/strategy/cb-lcars-strategy.js
+++ src/strategy/cb-lcars-strategy.js
@@ -3,7 +3,7 @@
 import * as CBLCARS from '../cb-lcars-vars.js'
 import { cblcarsLog } from '../utils/cb-lcars-logging.js';
 import { readYamlFile } from '../utils/cb-lcars-fileutils.js';
-import { path } from 'path';
+import { path } from 'path-browserify';
 
 export class CBLCARSDashboardStrategy {
     static async generate(config, hass) {
