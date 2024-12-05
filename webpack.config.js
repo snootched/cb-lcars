cd04e833f833c1be8eb9b78a6a27d137f656e1c4
Jay <snootched@gmail.com>
2024-12-04 20:41:09 -0500

    webpack testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/cb-lcars.js',
-    output: {
-        filename: 'cb-lcars.js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: './src/cb-lcars.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    devtool: 'source-map',
-    cache: false,
+  },
+  devtool: 'source-map',
 };
