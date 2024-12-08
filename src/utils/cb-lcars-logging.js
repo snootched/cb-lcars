0ac218e909663fddc7e51763bb5cd0eac69e3cbc
Jay <snootched@gmail.com>
2024-12-08 17:47:18 -0500

    global log level window var

--- src/utils/cb-lcars-logging.js
+++ src/utils/cb-lcars-logging.js
@@ -1,9 +1,10 @@
 import * as CBLCARS from '../cb-lcars-vars.js';
 
-let cblcarsGlobalLogLevel = 'info';
+let cblcarsGlobalLogLevel = window.cblcars.globalLogLevel || 'info';
 
 export function cblcarsSetGlobalLogLevel(level) {
   cblcarsGlobalLogLevel = level;
+  window.cblcars.globalLogLevel = level;
   cblcarsLog('info',`Setting CBLCARS global log level set to: ${level}`, {}, 'info');
 }
 export function cblcarsGetGlobalLogLevel() {
