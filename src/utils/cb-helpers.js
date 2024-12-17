8c5fbc345278ab6e083b5fa05fb0a4e63db48029
Jay <snootched@gmail.com>
2024-12-17 18:28:51 -0500

    let there be templates

--- src/utils/cb-helpers.js
+++ src/utils/cb-helpers.js
@@ -22,7 +22,7 @@ export function getLovelace() {
     let isUsingLovelaceTemplate = false;
     let overriddenTemplates = [];
 
-    templates = config.templates || [];
+    let templates = config.templates || [];
 
     for (const template of templates) {
         if (lovelaceTemplates.hasOwnProperty(template)) {
