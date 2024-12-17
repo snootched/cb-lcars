9bff4dc4adb212b2eed14c66a8b808470fb5c073
Jay <snootched@gmail.com>
2024-12-17 15:13:20 -0500

    fix missing function

--- src/cb-lcars.js
+++ src/cb-lcars.js
@@ -137,6 +137,24 @@ async function loadStubConfig(filePath) {
     }
 }
 
+function getLovelace() {
+    let root = document.querySelector('home-assistant');
+    root = root && root.shadowRoot;
+    root = root && root.querySelector('home-assistant-main');
+    root = root && root.shadowRoot;
+    root = root && root.querySelector('app-drawer-layout partial-panel-resolver, ha-drawer partial-panel-resolver');
+    root = (root && root.shadowRoot) || root;
+    root = root && root.querySelector('ha-panel-lovelace');
+    root = root && root.shadowRoot;
+    root = root && root.querySelector('hui-root');
+    if (root) {
+      const ll = root.lovelace;
+      ll.current_view = root.___curView;
+      return ll;
+    }
+    return null;
+  }
+
 
 class CBLCARSBaseCard extends ButtonCard {
 
